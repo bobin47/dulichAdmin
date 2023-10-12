2b6bf27e8d30f7ace524ab4942840cbda36e0b52
hoangchuong <lehoangchuong.lk@gmail.com>
2023-10-13 00:38:59 +0700

    delete code

--- src/pages/RoadMap/RoadMap.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function RoadMap() {
-  return <div>RoadMap</div>;
-}
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/auth/authSlice";
-import courseSlice from "./features/CourseSlice/CourseSlice";
-import UserSlice from "./features/UserSlice/UserSlice";
 import tourSlice from './features/tour/tourSlice';
 import dataFooterSlice from './features/DataFooter/dataFooter';
 import dataHeaderSlice from './features/DataHeader/dataHeader';
@@ -10,8 +8,6 @@ import dataHeaderSlice from './features/DataHeader/dataHeader';
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    course: courseSlice,
-    user: UserSlice,
     tour: tourSlice,
     dataFooter: dataFooterSlice,
     dataHeader:  dataHeaderSlice,
